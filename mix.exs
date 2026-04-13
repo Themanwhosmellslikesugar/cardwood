b792554a469588bdab50cc74475d2cd23276661a
Dmitrii Ivanko <tmwsls12@gmail.com>
2026-04-13 22:07:50 +0300

    Add run credo, dialyzer and sobelow in precommit task

--- mix.exs
+++ mix.exs
@@ -105,7 +105,15 @@ defmodule Cardwood.MixProject do
         "esbuild cardwood --minify",
         "phx.digest"
       ],
-      precommit: ["compile --warnings-as-errors", "deps.unlock --unused", "format", "test"]
+      precommit: [
+        "compile --warnings-as-errors",
+        "deps.unlock --unused",
+        "format",
+        "credo --strict",
+        "dialyzer",
+        "sobelow",
+        "test"
+      ]
     ]
   end
 end
